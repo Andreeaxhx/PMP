799906584da36bbc27622dcdbbf2464a59faf096
Andreea Pădurariu <62242417+Andreeaxhx@users.noreply.github.com>
2020-10-15 09:52:39 +0300

    covid

---
+++ Lab3/Lab3/Covid.scala
@@ -0,0 +1,29 @@
+package Lab3
+import com.cra.figaro.algorithm.factored._
+import com.cra.figaro.language._
+import com.cra.figaro.library.compound._
+
+class Covid{
+  Universe.createNew()
+  private val tuse = Flip(0.04)
+  private val febra = Flip(0.06)
+
+  private val covid = CPD(tuse, febra,
+    (false, false) -> Flip(0.25),
+    (false, true) -> Flip(0.4),
+    (true, false) -> Flip(0.5),
+    (true, true) -> Flip(0.8))
+
+  private val aiCovid = CPD(covid,
+    false -> Flip(0.01),
+    true -> Flip(0.7))
+
+  def main(args: Array[String]) {
+    aiCovid.observe(true)
+    val alg = VariableElimination(tuse, febra)
+    alg.start()
+    println("Probability of tuse: " + alg.probability(tuse, true))
+    println("Probability of tuse: " + alg.probability(febra, true))
+    alg.kill
+  }
+}
---
+++ Lab3/Lab3/HelloWorldTest.scala
@@ -0,0 +1,16 @@
+package Lab3
+import com.cra.figaro.language._
+import com.cra.figaro.algorithm.sampling._
+object HelloWorldTest{
+  def main(args: Array[String]){
+    val helloWorldElement = Constant("Hello world!")
+    val sampleHelloWorld = Importance(1000, helloWorldElement)
+
+    sampleHelloWorld.start()
+
+    println("Probability of Hello world:")
+    println(sampleHelloWorld.probability(helloWorldElement, "Hello world!"))
+    println("Probability of Goodbye world:")
+    println(sampleHelloWorld.probability(helloWorldElement, "Goodbye world!"))
+  }
+}
